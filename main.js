b0c5ce6302224550161b808635b1b6b6b10abb79
SekaliPakaiBuang <99380177+SekaliPakaiBuang@users.noreply.github.com>
2023-03-12 09:33:55 +0700

    Ganti cara select element & hapus console log (lupa)

--- main.js
+++ main.js
@@ -1,43 +1,34 @@
 // Elements and variables
-const dateLabel = document.getElementById("date");
-const separator = document.getElementsByClassName("separator");
+const dateLabel = document.querySelector("#date");
+const separator = document.querySelectorAll(".separator");
 
-const hourLabel = document.getElementById("hour");
-const minuteLabel = document.getElementById("minute");
-const secondLabel = document.getElementById("second");
+const hourLabel = document.querySelector("#hour");
+const minuteLabel = document.querySelector("#minute");
+const secondLabel = document.querySelector("#second");
 
-const secondsBar = document.getElementById("seconds-bar");
-const secondsProgress = document.getElementById("seconds-progress");
+const secondsBar = document.querySelector("#seconds-bar");
+const secondsProgress = document.querySelector("#seconds-progress");
 
 // Everlasting Loop Function
 function loop() {
     let now = new Date();
 
-    let time = {
-        date: now.toDateString(),
-
-        hour: now.getHours(),
-        minute: now.getMinutes(),
-        second: now.getSeconds(),
-        millis: now.getMilliseconds(),
-    };
-
-    dateLabel.innerHTML = time.date;
+    dateLabel.innerHTML = now.toDateString();
 
     // Blink every second
-    for (let i=0; i<separator.length; i++) {
-        separator[i].style.opacity = `${(1000 - (time.millis % 1000)) / 1000}`;
-    }
+    separator.forEach(i => {
+        i.style.opacity = `${(1000 - (now.getMilliseconds() % 1000)) / 1000}`;
+    });
 
     // Display time
-    hourLabel.innerHTML = time.hour.toString().padStart(2, "0");
-    minuteLabel.innerHTML = time.minute.toString().padStart(2, "0");
-    secondLabel.innerHTML = time.second.toString().padStart(2, "0");
+    hourLabel.innerHTML = now.getHours().toString().padStart(2, "0");
+    minuteLabel.innerHTML = now.getMinutes().toString().padStart(2, "0");
+    secondLabel.innerHTML = now.getSeconds().toString().padStart(2, "0");
 
-    secondsProgress.style.width = `${((time.second * 1000 + time.millis) / 600)}%`;
+    secondsProgress.style.width = `${((now.getSeconds() * 1000 + now.getMilliseconds()) / 600)}%`;
 
     // Progress bar logic
-    switch (time.minute % 4) {
+    switch (now.getMinutes() % 4) {
         case 0:
             secondsBar.style.backgroundColor = "var(--bar-color-1)";
             secondsProgress.style.backgroundColor = "var(--bar-color-2)";
@@ -63,5 +54,4 @@ function loop() {
 }
 
 // Init
-console.log(separator);
-requestAnimationFrame(loop);
+loop();
